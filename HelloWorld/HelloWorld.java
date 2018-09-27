2b254b434faf5a2f197c67cff11b6feeaae0dff1
Elizabeth Rosso <lizprogrammer@intracitygeeks.org>
2018-09-26 20:50:47 -0400

    Java Helloworld

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -21,25 +21,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
- 
-
-	// Initialize like usual
-	CloudRail.setAppKey([CloudRail key]);
-
-	// final Social social = new Twitter(redirectReceiver, "[clientID]", "[clientSecret]", "[redirectUri]");
-	// final Social social = new Facebook(redirectReceiver, "[pageName]", "[clientID]", "[clientSecret]", "[redirectUri]", "[state]");
-	final Social social = new Facebook(redirectReceiver, "[clientID]", "[clientSecret]", "[redirectUri]", "[state]");
-
-	// Get the parameters
-	String message = "I love CloudRail!!";
-	InputStream image; // Retrieve an InputStream for image
-
-	// Make the request
-	social.postImage(message, image);
-
-	System.out.println("I posted an image!");
-
-
     }
 
 }
